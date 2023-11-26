f75dc0961165ef540991c68c1547cf39d453e7ee
Åke Wivänge <ake.wivange@pagero.com>
2023-11-26 01:53:05 +0100

    fix: baseUrl

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
   url: 'https://sun-dragon-cult.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/sun-dragon-cult/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
