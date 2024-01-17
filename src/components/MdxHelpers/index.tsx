40b697fa37944a77c8736c9eeb93931e0294f9d7
Åke Wivänge <ake@wivange.se>
2024-01-17 08:36:36 +0100

    fix: add github issue link component and change files to mdx

--- src/components/MdxHelpers/index.tsx
+++ src/components/MdxHelpers/index.tsx
@@ -8,3 +8,17 @@ export const LightInvertSvg = ({ src, width }): JSX.Element => (
 export const CenterIt = ({ children }) => <span className={styles.centerIt}>{children}</span>;
 
 export const ThemeInvert = ({ children }) => <span className={styles.lightInvert}>{children}</span>;
+
+export const GithubIssue = ({ issue, repo }) => (
+  <a
+    title="Related Github Issue"
+    className="theme-doc-version-badge badge badge--secondary"
+    href={`https://github.com/sun-dragon-cult/${repo}/issues/${issue}`}
+    target="_blank"
+  >
+    <label>
+      <img src={"/img/github-mark.svg"} width={"13.5px"} />
+      &nbsp;#{issue}
+    </label>
+  </a>
+);
--- src/theme/MDXComponents.ts
+++ src/theme/MDXComponents.ts
@@ -1,9 +1,16 @@
 import MDXComponents from "@theme-original/MDXComponents";
-import { CenterIt, LightInvertSvg } from "@site/src/components/MdxHelpers";
+import {
+  CenterIt,
+  GithubIssue,
+  LightInvertSvg,
+  ThemeInvert,
+} from "@site/src/components/MdxHelpers";
 
 // Components that should be usable in mdx without import
 export default {
   ...MDXComponents,
   LightInvertSvg,
   CenterIt,
+  GithubIssue,
+  ThemeInvert,
 };
